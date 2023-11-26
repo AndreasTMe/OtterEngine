f5ed614da79cabe9f00286aab139bc19bbdcf636
andreas.t.dev <atsimpan@gmail.com>
2023-11-26 14:27:23 +0000

    Updated pre-build.bat and run configuration in CLion.

--- pre-build.bat
+++ pre-build.bat
@@ -2,15 +2,20 @@
 REM Used in Debug only
 REM Compile shaders to SPIR-V
 
-if not exist "%cd%\bin\Debug-Windows\OtterEngine\Assets\Shaders" mkdir "%cd%\bin\Debug-Windows\OtterEngine\Assets\Shaders"
-if not exist "%cd%\bin\Debug-Windows\OtterEngine\Assets\Textures" mkdir "%cd%\bin\Debug-Windows\OtterEngine\Assets\Textures"
+echo "Building in %1 mode for the %2 platform..."
 
-xcopy /s "%cd%\Assets\Textures" "%cd%\bin\Debug-Windows\OtterEngine\Assets\Textures" /Y
+set IN_DIR="%cd%"
+set OUT_DIR="%cd%\bin\%1-%2\OtterEngine"
+
+if not exist %OUT_DIR%\Assets\Shaders mkdir %OUT_DIR%\Assets\Shaders
+if not exist %OUT_DIR%\Assets\Textures mkdir %OUT_DIR%\Assets\Textures
+
+xcopy /s %IN_DIR%\Assets\Textures %OUT_DIR%\Assets\Textures /Y
 
 echo "Compiling shaders..."
 
-set IN_SHADER_DIR="%cd%\Assets\Shaders"
-set OUT_SHADER_DIR="%cd%\bin\Debug-Windows\OtterEngine\Assets\Shaders"
+set IN_SHADER_DIR=%IN_DIR%\Assets\Shaders
+set OUT_SHADER_DIR=%OUT_DIR%\Assets\Shaders
 
 for %%f in (%IN_SHADER_DIR%\*.vert %IN_SHADER_DIR%\*.frag) do (
     if "%%~xf"==".vert" (
