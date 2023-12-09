734a4a4f9734af7c55a2703d26974ff4c3ce2b25
andreas.t.dev <atsimpan@gmail.com>
2023-12-09 14:35:44 +0000

    Added documentation for Layers.h

--- Otter/includes/Core/Layers.h
+++ Otter/includes/Core/Layers.h
@@ -8,18 +8,51 @@
 
 namespace Otter
 {
+    /**
+     * @brief The Layer class is an abstraction that represents a layer in an application.
+     */
     class Layer
     {
     public:
+        /**
+         * @brief Virtual destructor.
+         */
         virtual ~Layer() = default;
 
+        /**
+         * @brief Enables the layer.
+         *
+         * @note This function is virtual, so it can be overridden by derived classes if necessary.
+         */
         virtual void OnEnable() { m_IsEnabled = true; }
+
+        /**
+         * @brief Disables the layer.
+         *
+         * @note This function is virtual, so it can be overridden by derived classes if necessary.
+         */
         virtual void OnDisable() { m_IsEnabled = false; }
+
+        /**
+         * @brief Called on each update of the layer. Derived classes must implement this function
+         * to define their specific behavior during an update.
+         *
+         * @param step The time step between updates.
+         */
         virtual void OnUpdate(const TimeStep& step) = 0;
 
+        /**
+         * @brief Check if the layer is enabled.
+         *
+         * @return True if the layer is enabled, false otherwise.
+         */
         [[nodiscard]] OTR_INLINE bool IsEnabled() const { return m_IsEnabled; }
 
     protected:
+        /**
+         * @brief Explicit default constructor. The layer is enabled by default, so if it must be disabled on creation,
+         * the user must pass false as argument. Otherwise, it can be left empty.
+         */
         explicit Layer(bool isEnabled = true)
             : m_IsEnabled(isEnabled)
         {
