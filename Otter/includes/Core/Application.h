4cf3f5d534ff45d9dcc0095ff5932532b8ff28c3
andreas.t.dev <atsimpan@gmail.com>
2023-12-11 00:01:13 +0000

    Added documentation for the Time class, Global Actions and the main Application
    - closes #35

--- Otter/includes/Core/Application.h
+++ Otter/includes/Core/Application.h
@@ -8,6 +8,10 @@
 
 namespace Otter
 {
+    /**
+     * @brief Configuration structure, used to pass information to the application, like window information,
+     * memory requirements, etc.
+     */
     struct ApplicationConfiguration final
     {
         const char* Title;
@@ -16,15 +20,43 @@ namespace Otter
         UInt64 MemoryRequirements;
     };
 
+    /**
+     * @brief Application class.
+     *
+     * @note This class is the main class of the engine. It is responsible for the initialization of
+     * the engine and the main loop.
+     */
     class Application
     {
     public:
+        /**
+         * @brief Constructor.
+         *
+         * @param config The application configuration.
+         */
         explicit Application(const ApplicationConfiguration& config);
+
+        /**
+         * @brief Destructor.
+         */
         virtual ~Application();
 
+        /**
+         * @brief Runs the application.
+         *
+         * @note This function is responsible for the main loop of the engine.
+         */
         void Run();
 
     protected:
+        /**
+         * @brief Used from child classes to push a layer to the application.
+         *
+         * @tparam TLayer An application layer.
+         * @tparam Args The types of arguments to pass to the layer constructor.
+         *
+         * @param args The arguments to pass to the layer constructor.
+         */
         template<typename TLayer, typename... Args>
         requires IsDerivedFrom<TLayer, Layer>
         void PushLayer(Args&& ... args)
@@ -39,6 +71,13 @@ namespace Otter
         List<Layer*> m_Layers{ };
     };
 
+    /**
+     * @brief Creates an application.
+     *
+     * @return A pointer to the application.
+     *
+     * @note This function is implemented by the client.
+     */
     [[nodiscard]] Application* CreateApplication();
 }
 
